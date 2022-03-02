f5e1c65352c101cce73177559fb0f516be99fee9
X-MENYASAH <100504860+X-MENYASAH@users.noreply.github.com>
2022-03-02 19:32:28 +0530

    Add files via upload

---
+++ area of rectangle.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+	
+	int length,breadth,areaofrectangle;           //declaration
+	
+	length=50;
+ 	breadth=60;                                  //initialization
+ 	
+ 	areaofrectangle=length*breadth;              //logic
+ 	
+ 	printf("%d",areaofrectangle);               //output
+ 	
+ 	return 0;
+	
+}
